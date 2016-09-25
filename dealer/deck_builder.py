7ef4991dc53945507b9826ab187126a2e096a647
Mark Dessain <mark.dessain@gmail.com>
2016-09-25 20:26:38 +0100

    adding new UI, adding in colour detection to limt down the options, adding multiprocessing and making a vision class for the training and webcam.

--- dealer/deck_builder.py
+++ dealer/deck_builder.py
@@ -1,22 +1,15 @@
-from collections import Counter
 
 
 class DeckBuilder:
 
     def __init__(self):
-        self.counter = Counter()
         self.deck = []
 
-    def update(self):
-        if sum(self.counter.values()) == 10:
-            print(self.counter)
-            self.counter.clear()
+    def undo(self):
+        if self.deck:
+            self.deck.pop()
 
-    def add(self, card):
-        self.counter[card] += 1
-
-    def select(self):
-        card = self.counter.most_common()[0][0]
+    def select(self, card):
         if card not in self.deck:
             self.deck.append(card)
 
@@ -40,7 +33,6 @@ class DeckBuilder:
             print('*** Hand 4 ***')
             print('**************')
             self.save()
-            self.counter.clear()
             self.deck = []
 
     def save(self):
--- dealer/main.py
+++ dealer/main.py
@@ -1,42 +1,6 @@
 
 import cv2
 
-from recognition import Recognition
-from webcam import Webcam
-from deck_builder import DeckBuilder
-
-from utils import process
-
-
 if __name__ == '__main__':
-
-
-    db = DeckBuilder()
-
-    c = Recognition(db)
-    c.train()
-    webcam = Webcam()
-
-
-    cv2.namedWindow("preview")
-    while True:
-        db.update()
-        webcam.update()
-
-        frame = webcam.frame
-        output = process(frame)
-
-        try:
-            db.add(c.guess(frame))
-        except:
-            pass
-
-        cv2.imshow("preview", output)
-
-        key = cv2.waitKey(20)
-        if key == 27: # exit on ESC
-            break
-        if ((key == 13) or (key==32)):
-            db.select()
-
-    cv2.destroyWindow("preview")
+    from ui import UI
+    UI().show()
